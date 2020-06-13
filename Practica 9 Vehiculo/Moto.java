2226cfbe2cadaa2e678904e72e89b7c2c85aa564
AxelJMV <63079109+AxelJMV@users.noreply.github.com>
2020-06-13 14:35:29 -0500

    Practica 9

---
+++ Practica 9 Vehiculo/Moto.java
@@ -0,0 +1,19 @@
+public class Moto extends Vehiculo{
+    int altura;
+    int ancho; 
+     public int getAltura(){
+        return altura;
+    }
+
+    public void setAltura(int altura){
+        this.altura=altura;
+    }
+    public int getAncho(){
+        return altura;
+    }
+
+    public void setAncho(int ancho){
+        this.ancho=ancho;
+    }
+
+}
